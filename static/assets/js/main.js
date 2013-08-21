dd8dd6b5dac504044266955c9d37f4a15202aa8e
David Chapman <d.chapman230@gmail.com>
2013-08-21 00:58:39 -0400

    mostly styled boilerplate about page, needs contact form, moving header, scrollto

---
+++ static/assets/js/main.js
@@ -0,0 +1,19 @@
+$().ready(function() {
+/*    var winHeight,
+        newSecHeight;
+
+    $(window).on('resize', function() {
+        halfHeights();
+    });
+
+    var halfHeights = function() {
+        console.log('hi');
+
+        winHeight = $(window).height();
+        newSecHeight = .5 * winHeight;
+
+        $('.half-height').css({'height': newSecHeight});
+    };
+
+    halfHeights();*/
+});
